6afb8c6a5d10e2cf9828958076a085910621b5d7
KOUADIO-BHEGNIN OTHNIEL KUAWO ANATOLE <othniel.kouadio@gs2e.ci>
2024-02-22 15:48:47 +0000

    post schemas

--- src/models/example.ts
+++ src/models/example.ts
@@ -1,8 +1,25 @@
-type ExampleType = {
-    id: number,
-    name: string,
-    content: string,
-    important: boolean
-}
+import mongoose from 'mongoose'
+const ExampleSchema = new mongoose.Schema({
+    id : {
+        type: Number,
+        required: "ID is required !",
+    },
+    name: {
+        type: String,
+        required: "Name is required !",
+        minlength: 1,
+        maxlength: 100
+    },
+    content: {
+        type: String,
+        required: "Name is required !",
+        minlength: 1,
+        maxlength: 100
+    },
+    important: {
+        type: Boolean,
+        required: true
+    }
+})
 
-export default ExampleType;
+export default mongoose.model("POST", ExampleSchema);
